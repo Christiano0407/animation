7f0648dd812824b6f64613bdb0fe6edeed5cb599
Christian Velázquez <placebo0704@gmail.com>
2019-11-12 09:42:03 -0600

    rebote

--- javascript/animador.js
+++ javascript/animador.js
@@ -1,1 +1,9 @@
-console.log("Alerta!");
+const $cuadrado = document.getElementById(`cuadrado`);
+
+$cuadrado.addEventListener(`animationend`, (event) => {
+// console.log(event, animationName);
+if(event.animationName === `rebote`) {
+ $cuadrado.style.animationName = `cuadrado escalera`;
+ $cuadrado.style.animationDuration = `3s`;
+}
+});
