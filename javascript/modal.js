cd3cbd1caa4237e5cadd63f7d5c487090473d547
Christian Velázquez <placebo0704@gmail.com>
2019-11-14 09:33:45 -0600

    modal2

--- javascript/modal.js
+++ javascript/modal.js
@@ -3,6 +3,8 @@ const $overlay = document.getElementById(`overlay`);
 const $modal = document.getElementById(`modal`);
 
 $hideButton.addEventListener(`click`, (event) => {
-    console.log(event);
-})
+    // console.log(event);
+    $modal.style.animation = `modalOut .8s forwards`;
+    $overlay.classList.remove(`active`);
+});
 
