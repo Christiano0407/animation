060843a3243a2aac555da04a543d5927f72a597a
Christian Velázquez <placebo0704@gmail.com>
2019-11-14 12:34:12 -0600

    heart

--- javascript/red.js
+++ javascript/red.js
@@ -3,5 +3,6 @@ const $like = document.getElementById(`like`);
 $like.addEventListener(`click`, (event) => {
 console.log(event);
 
-$like.classList.add(`is-liked`);
+// $like.classList.add(`is-liked`);
+$like.classList.toggle(`is-liked`);
 });
