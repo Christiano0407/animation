c26f68b2184988a4ddf27a0e73468f6d326c1149
Christian Velázquez <placebo0704@gmail.com>
2019-11-14 09:11:29 -0600

    event

---
+++ javascript/modal.js
@@ -0,0 +1,8 @@
+const $hideButton = document.getElementById(`hide-modal`);
+const $overlay = document.getElementById(`overlay`);
+const $modal = document.getElementById(`modal`);
+
+$hideButton.addEventListener(`click`, (event) => {
+    console.log(event);
+})
+
